0f0e423fc2da96196128d238226cde0f6061e409
rsf38 <rsimmons@f38.ca>
2017-05-02 15:06:13 -0400

    Update webpack.config.js
    Update to webpack 2 (hopefully).

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,20 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
+    rules: [{
+      test: /\.js$/
       exclude: /node_modules/,
-      loader: 'babel',
+      loader: 'babel-loader',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
-    }]
+    },
+            {
+              test: /\.scss$/,
+              loader: ['style-loader', 'css-loader', 'sass-loader']
+            }
+              
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
