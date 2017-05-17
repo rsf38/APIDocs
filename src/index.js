cb0eb2cd705c75f97e0314b1ad65ec79d0b26d48
Rob Simmons <rsimmons@f38.ca>
2017-05-17 16:49:51 -0400

    Remove settings.js since it apparently can't be ignored after added. Update initial files for API Docs. Pre development.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
 
 import App from './components/app';
 import reducers from './reducers';
+import FirebaseConfig from './settings'
 
 require('../style/style.scss');
+
+firebase.initializeApp(FirebaseConfig);
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
--- src/settings.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-  export const FirebaseConfig = {
-    apiKey: "api-key",
-    authDomain: "auth-domain",
-    databaseURL: "dburl",
-    projectId: "projectId",
-    storageBucket: "storageBucket",
-    messagingSenderId: "MessagingSenderId"
-  };
